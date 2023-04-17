af69ca03aa0caa728f35322a9de826c44e3660fc
DenchikSlaziet <106813642+DenchikSlaziet@users.noreply.github.com>
2023-04-17 20:11:49 +0300

    Добавьте файлы проекта.

---
+++ KDA_Task_01/Models/Seller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDA_Task_01.Models
+{
+    public class Seller
+    {
+        public User CurrentUser { get; set; }
+        private const int PRICE = 10;
+
+        public Seller(User currentUser)
+        {
+            CurrentUser = currentUser;
+        }
+
+        public int GetPrice() => PRICE;
+
+        public void Sell(int countCristal)
+        {
+            while(CurrentUser.GoldCount-PRICE>=0 && countCristal != 0)
+            {
+                CurrentUser.GoldCount -= PRICE;
+                CurrentUser.CristalCount++;
+                countCristal--;
+            }
+            Console.WriteLine("Продажа успешно выполнена");
+            PrintCheck();
+        }
+
+        private void PrintCheck()=> Console.WriteLine($"Кол-во золота = {CurrentUser.GoldCount}\nКол-во алмазов = {CurrentUser.CristalCount}\n");
+    }
+}
---
+++ KDA_Task_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using KDA_Task_01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDA_Task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User(GetParseInt("кол-во золота"));
+            var seller = new Seller(user);
+            Console.WriteLine($"Курс\n1 кристал = {seller.GetPrice()}");;
+            seller.Sell(GetParseInt("желаемое кол-во кристалов"));
+            Console.ReadLine();
+
+        }
+        public static int GetParseInt(string str)
+        {
+            int value;
+
+            Console.Write("Введите " + str + ": ");
+            while (!(Int32.TryParse(Console.ReadLine(), out value) && value > 0))
+            {
+                Console.WriteLine("Неверный формат!");
+                Console.Write("Введите " + str + ": ");
+            }
+
+            return value;
+        }
+    }
+}
